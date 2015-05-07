272fa31768c92ecdc38f8d9a13b03bfa26e1cd39
Thomas Wilburn <news@thomaswilburn.net>
2015-05-07 11:53:01 -0700

    Style cleanup, starting to bring in the ingredients

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,23 @@
 //Use CommonJS style via browserify to load other modules
 require("./lib/social");
 require("./lib/ads");
+
+var dot = require("dot");
+dot.templateSettings.varname = "data";
+dot.templateSettings.selfcontained = true;
+dot.templateSettings.evaluate = /<%([\s\S]+?)%>/g;
+dot.templateSettings.interpolate = /<%=([\s\S]+?)%>/g;
+
+var template = dot.template(require("./_dots.html"));
+
+var sections = Array.prototype.slice.call(document.querySelectorAll("section[id]"));
+var html = template({
+  sections: sections.map(function(section) {
+    var title = section.querySelector("h1, h2, h3");
+    return {
+      title: title ? title.innerHTML : ""
+    };
+  })
+});
+
+// console.log(html);
