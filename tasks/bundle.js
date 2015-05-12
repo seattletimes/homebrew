15c619e0096d0eb67218783a218e4e86d9fee834
Thomas Wilburn <news@thomaswilburn.net>
2015-05-12 10:55:01 -0700

    Added responsive-child code to the widget

--- tasks/bundle.js
+++ tasks/bundle.js
@@ -3,32 +3,42 @@ Build a bundled app.js file using browserify
 */
 module.exports = function(grunt) {
 
+  var async = require("async");
   var browserify = require("browserify");
   var exorcist = require("exorcist");
   var babel = require("babelify");
   var fs = require("fs");
+
+  var bundles = [
+    { src: "./src/js/main.js", dest: "build/app.js" },
+    { src: "./src/js/widget.js", dest: "build/widget.js" }
+  ];
 
   grunt.registerTask("bundle", "Build app.js using browserify", function(mode) {
     //run in dev mode unless otherwise specified
     mode = mode || "dev";
     var done = this.async();
 
-    var b = browserify({ debug: mode == "dev" });
-    b.transform(babel);
+    async.each(bundles, function(config, c) {
 
-    //make sure build/ exists
-    grunt.file.mkdir("build");
-    var output = fs.createWriteStream("build/app.js");
+      var b = browserify({ debug: mode == "dev" });
+      b.transform(babel);
 
-    b.add("./src/js/main.js");
-    var assembly = b.bundle();
-    if (mode == "dev") {
-      //output sourcemap
-      assembly = assembly.pipe(exorcist("build/app.js.map"));
-    }
-    assembly.pipe(output).on("finish", function() {
-      done();
-    });
+      //make sure build/ exists
+      grunt.file.mkdir("build");
+      var output = fs.createWriteStream(config.dest);
+
+      b.add(config.src);
+      var assembly = b.bundle();
+      if (mode == "dev") {
+        //output sourcemap
+        assembly = assembly.pipe(exorcist(config.dest + ".map"));
+      }
+      assembly.pipe(output).on("finish", function() {
+        c();
+      });
+
+    }, done);
 
   });
 
